cccf12dc0b05bdee4fc718fa8a473e87e730cc40
Chuck Muckamuck <chuck.muckamuck@gmail.com>
2018-10-28 21:34:26 -0500

    Bump version: 1.0.0 → 1.0.1

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 setup(
     name="example",
-    version="1.0.0",
+    version="1.0.1",
     packages=find_packages(),
     description='Python Example Module',
     author='chuck.muckamuck@gmail.com',
