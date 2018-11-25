1ec1428d465fa9438b5e155bfc3f61a42400da8f
Chuck Muckamuck <chuck.muckamuck@gmail.com>
2018-11-24 19:54:38 -0600

    add bumpversion to requirements

---
+++ build.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -e
+
+# Be in control of our location
+cd $(dirname ${0})
+
+# Make a temporrart virtualenv
+food=$(date +%s)
+virtualenv /tmp/${food}
+. /tmp/${food}/bin/activate
+pip install -Ur deployment/requirements.txt
+
+# Build the stacks
+bumpversion patch
+(cd vpc; stackility upsert -i config.ini)
+(cd iam/deploy; stackility upsert -i config.ini)
+(cd iam/project; stackility upsert -i config.ini)
+(cd deployment; stackility upsert -i config/prod.ini)
